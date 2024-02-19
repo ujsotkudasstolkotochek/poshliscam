0ede354c2150a373d4fc4de4400dde8cd205e975
ujsotkudasstolkotochek <160528845+ujsotkudasstolkotochek@users.noreply.github.com>
2024-02-19 21:51:35 +0300

    Create script.js

---
+++ project/new_project/templates/script.js
@@ -0,0 +1,6 @@
+let results = document.getElementById("results");
+
+document.getElementsByClassName("button").addEventListener('click', () => {
+    results.innerText = "Научись играть";
+    alert("кышмыш");
+});
