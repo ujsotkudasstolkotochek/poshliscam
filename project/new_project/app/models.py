bff55ce426fc3dcf5208859048e9cf5e92bc6f49
ujsotkudasstolkotochek <160528845+ujsotkudasstolkotochek@users.noreply.github.com>
2024-02-20 22:32:40 +0300

    Update models.py

--- project/new_project/app/models.py
+++ project/new_project/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Rabotnichki(models.Model):
+    name = models.CharField(max_length = 999, blank = False)
+    second_name = models.CharField(max_length = 999, blank = True)
+    third_name = models.CharField(max_length = 999, blank = True)
+    selery = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return f'{self.name} {self.second_name}'
