91133de2ddf38ef61c57307788359b354d44872e
ujsotkudasstolkotochek <160528845+ujsotkudasstolkotochek@users.noreply.github.com>
2024-02-20 22:32:53 +0300

    Update views.py

--- project/new_project/app/views.py
+++ project/new_project/app/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from app.models import Rabotnichki
 
 def index_page(request):
     return render(request, 'index.html')
+
+def about_page(request):
+    return render(request, 'about.html', context = {'workers_data' : Rabotnichki.objects.all()})
