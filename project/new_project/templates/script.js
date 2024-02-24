994216f865a2663808020d392b867d25bf72051a
ujsotkudasstolkotochek <160528845+ujsotkudasstolkotochek@users.noreply.github.com>
2024-02-24 09:33:39 +0000

    Delete project/new_project/templates/script.js

--- project/new_project/templates/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('button1').addEventListener('click', function() {
-    document.getElementById('results').innerText = 'научись играть';
-});
-
-document.getElementById('button2').addEventListener('click', function() {
-    document.getElementById('results').innerText = 'научись играть';
-});
