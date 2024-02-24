2dafeee10b5d7f617a46a3d405890e48afcc2deb
ujsotkudasstolkotochek <160528845+ujsotkudasstolkotochek@users.noreply.github.com>
2024-02-24 09:31:59 +0000

    Update views.py

--- project/new_project/app/views.py
+++ project/new_project/app/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
-from app.models import Rabotnichki
+from django.shortcuts import render, redirect
+from .models import Bin
+from .forms import BinForm
 
 def index_page(request):
     return render(request, 'index.html')
 
-def about_page(request):
-    return render(request, 'about.html', context = {'workers_data' : Rabotnichki.objects.all()})
+def create_page(request):
+    form = BinForm()
+    if request.method == 'POST':
+        form = BimForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('bin_page')
+    return render(request, 'create.html', context = {'form' : form})
+
+def bin_page(request, pk):
+    return render(request, 'bin_page', context = {'bin_data' : Bin.objects.get(id = pk)})
