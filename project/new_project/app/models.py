34596b0e5df85fd396be934555d69c675ccd38f5
ujsotkudasstolkotochek <160528845+ujsotkudasstolkotochek@users.noreply.github.com>
2024-02-24 09:31:42 +0000

    Update models.py

--- project/new_project/app/models.py
+++ project/new_project/app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Rabotnichki(models.Model):
+class Workers(models.Model):
     name = models.CharField(max_length = 999, blank = False)
     second_name = models.CharField(max_length = 999, blank = True)
     third_name = models.CharField(max_length = 999, blank = True)
@@ -8,3 +8,10 @@ class Rabotnichki(models.Model):
 
     def __str__(self):
         return f'{self.name} {self.second_name}'
+
+class Bin(models.Model):
+    name = models.CharField(max_length = 999, blank = False)
+    description = models.TextField(null = False)
+
+    def __str__(self):
+        return self.name
